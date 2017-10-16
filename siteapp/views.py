85324a76306b0862c365a9e3d1f4e1be8536ded4
tim stimpson <timothy.stimpson@gmail.com>
2017-10-16 16:56:48 +0100

    cleaned up views.py

--- siteapp/views.py
+++ siteapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.decorators.clickjacking import xframe_options_exempt
 
+
 @xframe_options_exempt
 def index(request):
-# this is your new view
-	return render(request, 'index.html')
+    return render(request, 'index.html')
